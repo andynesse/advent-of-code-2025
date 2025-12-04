78a16ca365cd86779d30d13ee5e054288eb562c5
andynesse <ann.nesse@stud.uis.no>
2025-12-04 11:30:42 +0100

    fix: Update part 1 on day 4

--- day_4/part_1.py
+++ day_4/part_1.py
@@ -23,27 +23,7 @@ def main():
                             adjacent += 1
                 if adjacent < 4:
                     tot_rolls += 1
-    print(tot_rolls)
-    with open("test.txt", mode="w"):
-        canvas = ""
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] == 1:
-                    adjacent = 0
-                    for y in range(max(i-1, 0), min(i+2, len(matrix))):
-                        for x in range(max(j-1, 0), min(j+2, len(matrix[0]))):
-                            if y == i and x == j:
-                                continue
-                            if matrix[y][x] == 1:
-                                adjacent += 1
-                    if adjacent < 4:
-                        canvas += "x"
-                        continue
-                    canvas += "@"
-                    continue
-                canvas += "."
-
-        
+    print(tot_rolls)    
 
 if __name__ == "__main__":
     main()
