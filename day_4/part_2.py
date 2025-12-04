97862a82eedbd25c444ca1bc31231c5960df8a75
andynesse <ann.nesse@stud.uis.no>
2025-12-04 11:17:05 +0100

    feat: Add code for day_4

---
+++ day_4/part_2.py
@@ -0,0 +1,34 @@
+def main():
+    matrix = []
+    with open("data.txt", mode="r") as f:
+        for line in f:
+            row = []
+            line = line.strip()
+            for char in line:
+                if char == "@":
+                    row.append(1)
+                    continue
+                row.append(0)
+            matrix.append(row)
+    tot_rolls = 0
+    while True:
+        part_rolls = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 1:
+                    adjacent = 0
+                    for y in range(max(i-1, 0), min(i+2, len(matrix))):
+                        for x in range(max(j-1, 0), min(j+2, len(matrix[0]))):
+                            if y == i and x == j:
+                                continue
+                            if matrix[y][x] == 1:
+                                adjacent += 1
+                    if adjacent < 4:
+                        matrix[i][j] = 0
+                        part_rolls += 1
+        if part_rolls == 0:
+            break
+        tot_rolls += part_rolls
+    print(tot_rolls)
+if __name__ == "__main__":
+    main()
